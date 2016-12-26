4ae5af1ba72d0eca3e6237c885b549038e40a603
gavinGuo <1536310027@qq.com>
2016-12-26 15:38:32 +0800

    build successful.

---
+++ proto/src/lib.rs
@@ -0,0 +1,31 @@
+extern crate ethcore_bigint as bigint;
+extern crate elastic_array;
+extern crate rustc_serialize;
+extern crate protobuf;
+
+use protobuf::Message;
+use protobuf::error::ProtobufResult;
+use protobuf::core::parse_from_bytes;
+
+
+mod recursive;
+
+#[macro_use]
+extern crate lazy_static;
+
+use elastic_array::ElasticArray1024;
+
+/*
+pub fn decode<T>(bytes: &[u8]) -> T where T: RlpDecodable {
+	let rlp = Rlp::new(bytes);
+	rlp.as_val()
+}
+
+pub fn encode<E>(object: &E) -> ElasticArray1024<u8> where E: RlpEncodable {
+	let mut stream = RlpStream::new();
+	stream.append(object);
+	stream.drain()
+}
+*/
+
+use recursive::{ListProto, RecursiveProto};
